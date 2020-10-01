2ea97a27a4beaf2b96152bbbba8e9acf3c6091fe
vrodedanya <socurahlem@yandex.ru>
2020-10-01 21:48:13 +0300

    Added cmake

---
+++ build/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/danil/IT/cpp/task/source/client.cpp" "/home/danil/IT/cpp/task/build/CMakeFiles/client.dir/home/danil/IT/cpp/task/source/client.cpp.o"
+  "/home/danil/IT/cpp/task/source/code.cpp" "/home/danil/IT/cpp/task/build/CMakeFiles/client.dir/home/danil/IT/cpp/task/source/code.cpp.o"
+  "/home/danil/IT/cpp/task/source/mclient.cpp" "/home/danil/IT/cpp/task/build/CMakeFiles/client.dir/home/danil/IT/cpp/task/source/mclient.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/home/danil/IT/cpp/task/source/code.cpp.o"
+  "CMakeFiles/server.dir/home/danil/IT/cpp/task/source/mserver.cpp.o"
+  "CMakeFiles/server.dir/home/danil/IT/cpp/task/source/server.cpp.o"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
